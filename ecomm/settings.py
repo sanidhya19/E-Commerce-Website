75732e0d721e726df59ef50a9286a8d415c807ce
sanidhya19 <khandelwalsanidhya19@gmail.com>
2025-04-17 12:50:43 +0530

    Dockerize

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+       BASE_DIR / 'static/',
+        ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
